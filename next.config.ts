62bf6c241279b323bf590c351006912aba4e2554
camcoder337 <camcoder337@gmail.com>
2025-08-07 09:05:37 +0100

    implementing builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
